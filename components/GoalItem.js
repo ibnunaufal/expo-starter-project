e116edcd774f4a4ea4a0df8c8043cb0fe50e3792
Ibnu Naufal <ibnunaufal5758@gmail.com>
2022-08-27 14:50:25 +0700

    add ripple effect & adjust on ios

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,7 +2,11 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
+    <Pressable
+      android_ripple={{ color: "#ddd" }}
+      onPress={props.onDeleteItem.bind(this, props.id)}
+      style={(pressData) => pressData.pressed && styles.pressItem}
+    >
       <View style={styles.goalItem}>
         <Text style={styles.goalText}> {props.text} </Text>
       </View>
@@ -14,11 +18,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
   },
   goalText: {
+    padding: 8,
     color: "white",
+  },
+  pressItem: {
+    opacity: 0.5,
   },
 });
